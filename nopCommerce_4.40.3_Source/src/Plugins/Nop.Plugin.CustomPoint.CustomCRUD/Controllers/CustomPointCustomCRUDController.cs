7cc00e33848f16d87708b7386a709913851704e2
Dia <dia.brightsoft@gmail.com>
2021-07-09 14:30:06 +0700

    source code

---
+++ nopCommerce_4.40.3_Source/src/Plugins/Nop.Plugin.CustomPoint.CustomCRUD/Controllers/CustomPointCustomCRUDController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Core.Domain.Discounts;
+using Nop.Plugin.CustomPoint.CustomerCRUD.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Customers;
+using Nop.Services.Discounts;
+using Nop.Services.Localization;
+using Nop.Services.Security;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Controllers;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.CustomPoint.CustomerCRUD.Controllers
+{
+    [AuthorizeAdmin]
+    [Area(AreaNames.Admin)]
+    [AutoValidateAntiforgeryToken]
+    public class CustomPointCustomerCRUDController : BasePluginController
+    {
+        #region Fields
+
+        private readonly ICustomerService _customerService;
+        private readonly IDiscountService _discountService;
+        private readonly ILocalizationService _localizationService;
+        private readonly IPermissionService _permissionService;
+        private readonly ISettingService _settingService;
+
+        #endregion
+
+        #region Ctor
+
+        public CustomPointCustomerCRUDController(ICustomerService customerService,
+            IDiscountService discountService,
+            ILocalizationService localizationService,
+            IPermissionService permissionService,
+            ISettingService settingService)
+        {
+            _customerService = customerService;
+            _discountService = discountService;
+            _localizationService = localizationService;
+            _permissionService = permissionService;
+            _settingService = settingService;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<IActionResult> Configure(int discountId, int? discountRequirementId)
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManageDiscounts))
+                return Content("Access denied");
+
+            //load the discount
+            var discount = await _discountService.GetDiscountByIdAsync(discountId);
+            if (discount == null)
+                throw new ArgumentException("Discount could not be loaded");
+
+            //check whether the discount requirement exists
+            if (discountRequirementId.HasValue && await _discountService.GetDiscountRequirementByIdAsync(discountRequirementId.Value) is null)
+                return Content("Failed to load requirement.");
+
+            var model = new RequirementModel
+            {
+                RequirementId = discountRequirementId ?? 0,
+                DiscountId = discountId
+            };
+
+            return View("~/Plugins/CustomPoint.CustomCRUD/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]        
+        public async Task<IActionResult> Configure(RequirementModel model)
+        {
+            if (!await _permissionService.AuthorizeAsync(StandardPermissionProvider.ManageDiscounts))
+                return Content("Access denied");
+
+            if (ModelState.IsValid)
+            {
+                //load the discount
+                var discount = await _discountService.GetDiscountByIdAsync(model.DiscountId);
+                if (discount == null)
+                    return NotFound(new { Errors = new[] { "Discount could not be loaded" } });
+
+                //get the discount requirement
+                var discountRequirement = await _discountService.GetDiscountRequirementByIdAsync(model.RequirementId);
+
+                return Ok(new { NewRequirementId = discountRequirement.Id });
+            }
+
+            return Ok(new { Errors = GetErrorsFromModelState(ModelState) });
+        }
+
+        #endregion
+
+        #region Utilities
+
+        private IEnumerable<string> GetErrorsFromModelState(ModelStateDictionary modelState)
+        {
+            return ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+        }
+
+        #endregion
+    }
+}
